7c46e6cad8d9c312b5c5764794759c822c9d09f2
LuisMartinez2021 <luis79624523@gmail.com>
2024-08-08 08:59:12 -0400

    Clase 06

--- pos_academy.sql
+++ pos_academy.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-07-2024 a las 13:37:03
+-- Tiempo de generación: 08-08-2024 a las 14:56:09
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -20,6 +20,88 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pos_academy`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `id_cliente` int(11) NOT NULL,
+  `razon_social_cliente` varchar(100) NOT NULL,
+  `nit_ci_cliente` varchar(50) NOT NULL,
+  `direccion_cliente` varchar(100) NOT NULL,
+  `nombre_cliente` varchar(100) NOT NULL,
+  `telefono_cliente` varchar(50) NOT NULL,
+  `email_cliente` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cufd`
+--
+
+CREATE TABLE `cufd` (
+  `id_cufd` int(11) NOT NULL,
+  `codigo_cufd` varchar(200) NOT NULL,
+  `codigo_control` varchar(50) NOT NULL,
+  `fecha_vigencia` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id_factura` int(11) NOT NULL,
+  `cod_factura` varchar(30) NOT NULL,
+  `id_cliente` int(11) NOT NULL,
+  `detalle` text NOT NULL,
+  `neto` decimal(10,2) NOT NULL,
+  `descuento` decimal(10,2) NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `fecha_emision` datetime NOT NULL,
+  `cufd` varchar(100) NOT NULL,
+  `cuf` varchar(200) NOT NULL,
+  `xml` text NOT NULL,
+  `id_punto_venta` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `leyenda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `leyenda`
+--
+
+CREATE TABLE `leyenda` (
+  `id_leyenda` int(11) NOT NULL,
+  `desc_leyenda` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `id_producto` int(11) NOT NULL,
+  `cod_producto` varchar(50) NOT NULL,
+  `cod_producto_sin` int(11) NOT NULL,
+  `nombre_producto` varchar(100) NOT NULL,
+  `precio_producto` decimal(10,2) NOT NULL,
+  `unidad_medida` varchar(30) NOT NULL,
+  `unidad_medida_sin` int(11) NOT NULL,
+  `imagen_producto` varchar(50) NOT NULL,
+  `disponible` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -42,13 +124,42 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id_usuario`, `login_usuario`, `password`, `perfil`, `estado`, `ultimo_login`, `fecha_registro`) VALUES
-(1, 'admin', '$2y$10$0Iufo/3cELZshSjFytK.H.61ZJO4KdktDOmGTxN7j3wSUZlZ4jZwS', 'Administrador', 1, '2024-07-29 20:29:21', '2024-07-25 04:00:00'),
-(2, 'luis', '$2y$10$0Iufo/3cELZshSjFytK.H.61ZJO4KdktDOmGTxN7j3wSUZlZ4jZwS', 'Moderador', 1, '2024-07-29 18:55:06', '2024-07-29 22:50:34'),
-(3, 'moderador', '$2y$10$XjV7u/YjKkGfDxkiQceNh.COr/cC0UZdDh597j7fiZSxKSYmgtPve', 'Moderador', 1, '2024-07-29 18:56:58', '2024-07-29 22:50:15');
+(1, 'admin', '$2y$10$0Iufo/3cELZshSjFytK.H.61ZJO4KdktDOmGTxN7j3wSUZlZ4jZwS', 'Administrador', 1, '2024-08-04 18:31:38', '2024-07-25 04:00:00'),
+(2, 'luis', '$2y$10$1xt81f4GnR4cpzWKQhSgyehMuppNt6EiRp4hPdTuZevfUWtB8NDx.', 'Moderador', 1, '2024-08-03 10:51:32', '2024-07-29 22:50:34');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`id_cliente`);
+
+--
+-- Indices de la tabla `cufd`
+--
+ALTER TABLE `cufd`
+  ADD PRIMARY KEY (`id_cufd`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id_factura`);
+
+--
+-- Indices de la tabla `leyenda`
+--
+ALTER TABLE `leyenda`
+  ADD PRIMARY KEY (`id_leyenda`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`id_producto`);
 
 --
 -- Indices de la tabla `usuario`
@@ -61,10 +172,40 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cufd`
+--
+ALTER TABLE `cufd`
+  MODIFY `id_cufd` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id_factura` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `leyenda`
+--
+ALTER TABLE `leyenda`
+  MODIFY `id_leyenda` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
